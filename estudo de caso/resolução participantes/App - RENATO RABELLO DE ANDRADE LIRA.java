0ccc2420158ac8a815bf48a14e5b14aa2a5627f5
Felipe Veloso <felipe.veloso@liferay.com>
2022-12-01 21:09:21 -0300

    add estudo de caso

---
+++ estudo de caso/resolução participantes/App - RENATO RABELLO DE ANDRADE LIRA.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import org.eclipse.jgit.api.Git;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+
+        int num = plusFive(10);
+
+    }
+
+    public static int plusFive(int num){
+
+        return num + 5;
+    }
+
+}
